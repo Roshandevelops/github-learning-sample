aa32f16f068605b5c14b395f84e244f603d9cb51
Roshan kk <roshanochu09@gmail.com>
2025-01-30 13:59:05 +0530

    i added my vs vscode project

---
+++ git_sample/lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Column(
+          children: [
+            SafeArea(
+                child: Center(
+                    child: Column(
+              children: [
+                Text("sample text"),
+              ],
+            )))
+          ],
+        ),
+      ),
+    );
+  }
+}
